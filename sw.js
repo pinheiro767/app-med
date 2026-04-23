9ec4534c4055ff2d606d68bdd6a1a5dd660ae8e5
pinheiro767 <claudiapinheiro767@gmail.com>
2026-04-23 15:54:54 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,18 +1,48 @@
-const CACHE_NAME = "mmii-pwa-v2";
+const CACHE_NAME = "auditoria-mmii-v2";
 
-self.addEventListener("install", event => {
+const URLS_TO_CACHE = [
+  "/",
+  "/manifest.json",
+  "/sw.js",
+  "/static/css/style.css",
+  "/static/js/database.js",
+  "/static/js/app.js",
+  "/static/icons/icon192.png",
+  "/static/icons/icon512.png"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+  );
   self.skipWaiting();
 });
 
-self.addEventListener("activate", event => {
+self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.map(key => caches.delete(key)))
+    caches.keys().then((keys) =>
+      Promise.all(
+        keys
+          .filter((key) => key !== CACHE_NAME)
+          .map((key) => caches.delete(key))
+      )
     )
   );
   self.clients.claim();
 });
 
-self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((cachedResponse) => {
+      if (cachedResponse) return cachedResponse;
+
+      return fetch(event.request)
+        .then((networkResponse) => {
+          return networkResponse;
+        })
+        .catch(() => {
+          return caches.match("/");
+        });
+    })
+  );
 });

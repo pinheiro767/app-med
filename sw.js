4bd2135e24a0205fea1e8e0207d87bb69c2af7ee
pinheiro767 <claudiapinheiro767@gmail.com>
2026-04-23 16:54:58 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,40 +1,48 @@
-const CACHE_NAME = "auditoria-mmii-v2";
+const CACHE_NAME = "avaliacao-anatomia-cache-v1";
 
 const URLS_TO_CACHE = [
   "/",
   "/manifest.json",
   "/sw.js",
   "/static/css/style.css",
-  "/static/js/database.js",
   "/static/js/app.js",
-  "/static/icons/icon192.png",
-  "/static/icons/icon512.png"
+  "/static/js/database.js",
+  "/static/icons/icon-192.png",
+  "/static/icons/icon-512.png",
+  "/static/icons/maskable-512.png"
 ];
 
+// instalar cache
 self.addEventListener("install", (event) => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => cache.addAll(URLS_TO_CACHE))
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(URLS_TO_CACHE);
+    })
   );
   self.skipWaiting();
 });
 
+// ativar
 self.addEventListener("activate", (event) => {
   event.waitUntil(
-    caches.keys().then((keys) =>
-      Promise.all(
+    caches.keys().then((keys) => {
+      return Promise.all(
         keys
           .filter((key) => key !== CACHE_NAME)
           .map((key) => caches.delete(key))
-      )
-    )
+      );
+    })
   );
   self.clients.claim();
 });
 
+// interceptar requisições
 self.addEventListener("fetch", (event) => {
   event.respondWith(
-    caches.match(event.request).then((cachedResponse) => {
-      if (cachedResponse) return cachedResponse;
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      }
 
       return fetch(event.request)
         .then((networkResponse) => {

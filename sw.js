477d39033657c925187302995d2863633e2002a1
pinheiro767 <claudiapinheiro767@gmail.com>
2026-04-28 03:06:30 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,14 +1,12 @@
-const CACHE_NAME = "avaliacao-anatomia-v22";
+const CACHE_NAME = "avaliacao-anatomia-v23";
 
 const URLS_TO_CACHE = [
   "/",
   "/index.html",
   "/manifest.json",
-
   "/static/css/style.css",
   "/static/js/app.js",
   "/static/js/database.js",
-
   "/static/icons/icon-192.png",
   "/static/icons/icon-512.png",
   "/static/icons/maskable-512.png"
@@ -16,32 +14,45 @@ const URLS_TO_CACHE = [
 
 self.addEventListener("install", event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
+    caches
+      .open(CACHE_NAME)
+      .then(cache => cache.addAll(URLS_TO_CACHE))
+      .then(() => self.skipWaiting())
   );
-
-  self.skipWaiting();
 });
 
 self.addEventListener("activate", event => {
   event.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(
-        keys
-          .filter(key => key !== CACHE_NAME)
-          .map(key => caches.delete(key))
+    caches
+      .keys()
+      .then(keys =>
+        Promise.all(
+          keys
+            .filter(key => key !== CACHE_NAME)
+            .map(key => caches.delete(key))
+        )
       )
-    )
+      .then(() => self.clients.claim())
   );
-
-  self.clients.claim();
 });
 
 self.addEventListener("fetch", event => {
   if (event.request.method !== "GET") return;
 
+  const url = new URL(event.request.url);
+
+  if (url.origin !== self.location.origin) {
+    event.respondWith(fetch(event.request));
+    return;
+  }
+
   event.respondWith(
     fetch(event.request)
       .then(response => {
+        if (!response || response.status !== 200) {
+          return response;
+        }
+
         const clone = response.clone();
 
         caches.open(CACHE_NAME).then(cache => {
@@ -50,10 +61,14 @@ self.addEventListener("fetch", event => {
 
         return response;
       })
-      .catch(() =>
-        caches.match(event.request).then(response => {
-          return response || caches.match("/") || caches.match("/index.html");
-        })
-      )
+      .catch(async () => {
+        const cachedResponse = await caches.match(event.request);
+
+        if (cachedResponse) {
+          return cachedResponse;
+        }
+
+        return caches.match("/") || caches.match("/index.html");
+      })
   );
 });

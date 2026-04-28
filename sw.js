127f9999e3453ea6916badcb71f928e1cb15c6e4
pinheiro767 <claudiapinheiro767@gmail.com>
2026-04-28 02:40:18 -0300

    Update service worker cache version and logic

--- sw.js
+++ sw.js
@@ -1,9 +1,9 @@
-const CACHE_NAME = "avaliacao-anatomia-v21";
+const CACHE_NAME = "avaliacao-anatomia-v22";
 
 const URLS_TO_CACHE = [
   "/",
+  "/index.html",
   "/manifest.json",
-  "/sw.js",
 
   "/static/css/style.css",
   "/static/js/app.js",
@@ -15,44 +15,45 @@ const URLS_TO_CACHE = [
 ];
 
 self.addEventListener("install", event => {
-
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => {
-      return cache.addAll(URLS_TO_CACHE);
-    })
+    caches.open(CACHE_NAME).then(cache => cache.addAll(URLS_TO_CACHE))
   );
 
   self.skipWaiting();
-
 });
 
 self.addEventListener("activate", event => {
-
   event.waitUntil(
-    caches.keys().then(keys => {
-
-      return Promise.all(
+    caches.keys().then(keys =>
+      Promise.all(
         keys
           .filter(key => key !== CACHE_NAME)
           .map(key => caches.delete(key))
-      );
-
-    })
+      )
+    )
   );
 
   self.clients.claim();
-
 });
 
 self.addEventListener("fetch", event => {
+  if (event.request.method !== "GET") return;
 
   event.respondWith(
-    caches.match(event.request).then(response => {
+    fetch(event.request)
+      .then(response => {
+        const clone = response.clone();
 
-      return response || fetch(event.request)
-        .catch(() => caches.match("/"));
+        caches.open(CACHE_NAME).then(cache => {
+          cache.put(event.request, clone);
+        });
 
-    })
+        return response;
+      })
+      .catch(() =>
+        caches.match(event.request).then(response => {
+          return response || caches.match("/") || caches.match("/index.html");
+        })
+      )
   );
-
 });
